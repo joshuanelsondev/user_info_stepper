2969218c67b32c3ea9d0bf5219b47b1dc2e07d52
joshuanelsondev <joshuanelsondev@gmail.com>
2024-09-15 13:39:16 -0400

    refactor: update AgeForm and NameForm components to use handleChange function for input fields

--- src/components/AgeForm.tsx
+++ src/components/AgeForm.tsx
@@ -5,11 +5,19 @@ interface AgeFormProps {
   updateUserInfo: (field: 'age', value: number) => void
 }
 
-export default function AgeForm({ setFormView }: AgeFormProps) {
+export default function AgeForm({
+  setFormView,
+  userInfo,
+  updateUserInfo,
+}: AgeFormProps) {
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = e.target
+    updateUserInfo('age', Number(value))
+  }
   return (
     <div>
       <label htmlFor="age">What is your age?</label>
-      <input type="age" />
+      <input type="age" value={userInfo.age} onChange={handleChange} />
 
       <button onClick={() => setFormView('name')}>Back</button>
       <button onClick={() => setFormView('review')}>Next</button>
--- src/components/NameForm.tsx
+++ src/components/NameForm.tsx
@@ -6,17 +6,32 @@ interface NameFormProps {
   updateUserInfo: (field: 'first_name' | 'last_name', value: string) => void
 }
 
-export default function NameForm({ setFormView }: NameFormProps) {
+export default function NameForm({
+  setFormView,
+  updateUserInfo,
+  userInfo,
+}: NameFormProps) {
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { id, value } = e.target
+    updateUserInfo(id as 'first_name' | 'last_name', value)
+  }
   return (
     <div>
       <label htmlFor="first_name">What is your first name?</label>
-      <input type="text" id="first_name" />
+      <input
+        type="text"
+        id="first_name"
+        value={userInfo.first_name}
+        onChange={handleChange}
+      />
 
       <label htmlFor="last_name">What is your last name?</label>
       <input type="text" id="last_name" />
 
       <button disabled>Back</button>
-      <button onClick={() => setFormView('age')}>Next</button>
+      <button type="button" onClick={() => setFormView('age')}>
+        Next
+      </button>
     </div>
   )
 }
