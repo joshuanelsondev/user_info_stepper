7d61e8c246568107be562dfa1ea065b09c83e6bf
joshuanelsondev <joshuanelsondev@gmail.com>
2024-09-14 23:58:58 -0400

    feat: handle form submission

--- src/components/ReviewUserInfo.tsx
+++ src/components/ReviewUserInfo.tsx
@@ -4,12 +4,28 @@ interface ReviewUserInfoProps {
   userInfo: { first_name: string; last_name: string; age: number }
 }
 
-export default function ReviewUserInfo({ setFormView }: ReviewUserInfoProps) {
+export default function ReviewUserInfo({
+  setFormView,
+  userInfo,
+}: ReviewUserInfoProps) {
+  const handleSubmit = (
+    e: React.FormEvent<HTMLFormElement>,
+    userInfo: ReviewUserInfoProps['userInfo']
+  ) => {
+    e.preventDefault()
+
+    localStorage.setItem('userInfo', JSON.stringify(userInfo))
+  }
+  console.log(userInfo)
   return (
-    <div>
+    <form onSubmit={(e) => handleSubmit(e, userInfo)}>
       <p>Review User Info</p>
+      <p>
+        Name: {userInfo.first_name} {userInfo.last_name}
+      </p>
+      <p>Age: {userInfo.age}</p>
       <button onClick={() => setFormView('age')}>Back</button>
-      <button>Finish</button>
-    </div>
+      <button type="submit">Finish</button>
+    </form>
   )
 }
